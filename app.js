6508b51d9142bf61f3fab34c8a675f6d3a3bc900
Matthew Suchy <mattsuchy@gmail.com>
2020-02-24 10:28:42 +0200

    Added Delete and filtering functionality

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //Define UI Vars
 const form = document.querySelector('#task-form');
-const taskList = document.querySelector('.collecion');
+const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
@@ -13,6 +13,12 @@ loadEventListeners();
 function loadEventListeners() {
     //Add Task Event
     form.addEventListener('submit', addTask);
+    //Remove task event
+    taskList.addEventListener('click', removeTask);
+    //Clear all tasks event
+    clearBtn.addEventListener('click', clearTasks);
+    //Filter Tasks Event
+    filter.addEventListener('keyup', filterTasks)
 }
 
 //Add Task
@@ -24,9 +30,53 @@ function addTask(e) {
     const li = document.createElement('li');
     //Add Class
     li.className = 'collection-item';
+    //Create txt node and append li
+    li.appendChild(document.createTextNode(taskInput.value));
+    //create new link element
+    const link = document.createElement('a');
+    //add class
+    link.className = 'delete-item right';
+    //add icon html
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    //Append link to li
+    li.appendChild(link);
+    //Append li to ul
+    taskList.appendChild(li);
 
-
-
+    //Clear input
+    taskInput.value = ''
 
     e.preventDefault();
+}
+
+//Remove task
+function removeTask(e) {
+    if (e.target.parentElement.classList.contains('delete-item')) {
+        if (confirm('Are you sure?')) {
+            e.target.parentElement.parentElement.remove();
+        }
+    }
+}
+
+//Clear all tasks
+function clearTasks() {
+    while (taskList.firstChild) {
+        taskList.removeChild(taskList.firstChild);
+    }
+}
+
+//Filter tasks
+function filterTasks(e) {
+    const text = e.target.value.toLowerCase();
+    document.querySelectorAll('.collection-item').forEach
+        (function (task) {
+            const item = task.firstChild.textContent;
+            if (item.toLowerCase().indexOf(text) != -1) {
+                task.style.display = 'block'
+            } else {
+                task.style.display = 'none'
+            }
+
+        });
+
 }
